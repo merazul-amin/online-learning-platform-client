6d0141661a83e6036da701f4cd73091af05a6818
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-24 21:17:39 +0600

    site name and logo updated

--- src/components/SharedPages/HeaderNav/HeaderNav.js
+++ src/components/SharedPages/HeaderNav/HeaderNav.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/UserContext/UserContext';
 
 const HeaderNav = () => {
+    const { user } = useContext(AuthContext);
+    console.log(user);
     return (
         <div>
             <h1>This is Navigation Bar</h1>
--- src/contexts/UserContext/UserContext.js
+++ src/contexts/UserContext/UserContext.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 
 const auth = getAuth(app);
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 const UserContext = ({ children }) => {
     const [user, setUser] = useState(null);
