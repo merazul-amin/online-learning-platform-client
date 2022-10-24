158387dbee20bf4e872f2405779a363f52073ecb
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-24 21:21:55 +0600

    navbar added

--- src/components/SharedPages/HeaderNav/HeaderNav.js
+++ src/components/SharedPages/HeaderNav/HeaderNav.js
@@ -1,13 +1,42 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../contexts/UserContext/UserContext';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const HeaderNav = () => {
     const { user } = useContext(AuthContext);
     console.log(user);
     return (
-        <div>
-            <h1>This is Navigation Bar</h1>
-        </div>
+        <Navbar bg="info" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">Online Learning</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">Courses</Nav.Link>
+                        <Nav.Link href="#action2">Blog</Nav.Link>
+
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
