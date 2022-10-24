7b4fcf2e3e2a0ad3ebdd890235cdf0986c35d657
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-24 20:51:45 +0600

    basic setup done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// bootstrap css link
+import 'bootstrap/dist/css/bootstrap.min.css';
+// toast css link
+import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
