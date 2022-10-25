6175115f230effef25d2c2110db5a1838519b0b6
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-25 16:13:26 +0600

    courses two side added

--- src/components/Pages/AllCourses/AllCourses.js
+++ src/components/Pages/AllCourses/AllCourses.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { useLoaderData } from "react-router-dom";
 import SingleCourse from '../SingleCourse/SingleCourse';
 import Row from 'react-bootstrap/Row';
-
+import Col from 'react-bootstrap/Col'
 const AllCourses = () => {
     const courses = useLoaderData();
     return (
-        <div className='w-75 mx-auto'>
-            <Row xs={1} md={2} className="g-4">
-                {
-                    courses.map(course =>
-                        <SingleCourse
-                            course={course}
-                            key={course.id}
-                        ></SingleCourse>)
-                }
-            </Row>
-        </div>
+        <Row className='w-75 mx-auto'>
+            <Col lg='10'>
+                <div >
+                    <Row xs={1} md={2} className="g-4">
+                        {
+                            courses.map(course =>
+                                <SingleCourse
+                                    course={course}
+                                    key={course.id}
+                                ></SingleCourse>)
+                        }
+                    </Row>
+                </div>
+            </Col>
+            <Col lg='2 border'>
+                <h1>Right nav</h1>
+            </Col>
+        </Row>
+
     );
 };
 
