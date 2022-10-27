19f719cda7ea418d8b5eb07c4cbc1bc8af8fc0a2
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-10-27 10:11:51 +0600

    all done

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -3,9 +3,10 @@ import { AuthContext } from '../../../contexts/UserContext/UserContext';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { toast } from 'react-toastify';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 const Register = () => {
+    const navigate = useNavigate();
     const { createUser, setNameImage } = useContext(AuthContext);
     const [uiError, setUiError] = useState('');
     const handleSubmit = (e) => {
@@ -21,6 +22,7 @@ const Register = () => {
                 // Signed in 
                 toast.success('Successfully Account Created')
                 setUiError('');
+                navigate('/');
 
                 // update name and phot url
                 setNameImage(userName, photoUrl)
@@ -44,25 +46,25 @@ const Register = () => {
             {/* Name input */}
             <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label className='fw-bold fs-3'>Full Name</Form.Label>
-                <Form.Control name='name' type="text" placeholder="Enter full Name" />
+                <Form.Control name='name' type="text" required placeholder="Enter full Name" />
             </Form.Group>
 
             {/* Photo url Input */}
             <Form.Group className="mb-3" controlId="formBasicPhoto">
                 <Form.Label className='fw-bold fs-3'>Your Photo Url</Form.Label>
-                <Form.Control name='photo' type="text" placeholder="Enter photo url" />
+                <Form.Control name='photo' required type="text" placeholder="Enter photo url" />
             </Form.Group>
 
             {/* Email input */}
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label className='fw-bold fs-3'>Email</Form.Label>
-                <Form.Control name='email' type="email" placeholder="Enter Your Email" />
+                <Form.Control required name='email' type="email" placeholder="Enter Your Email" />
             </Form.Group>
 
             {/* Password input */}
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label className='fw-bold fs-3'>Password</Form.Label>
-                <Form.Control name='password' type="password" placeholder="Password" />
+                <Form.Control required name='password' type="password" placeholder="Password" />
                 <Form.Text className="text-muted">
                     <span className='text-danger'>{uiError}</span>
                 </Form.Text>
